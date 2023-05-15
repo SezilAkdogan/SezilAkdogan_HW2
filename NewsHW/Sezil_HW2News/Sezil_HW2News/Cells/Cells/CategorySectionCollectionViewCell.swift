9b1b9b2cce709a185383c53b794a39e07d8952a8
Sezil Akdoğan <111681902+SezilAkdogan@users.noreply.github.com>
2023-05-16 02:34:49 +0300

    added CollectionViewCell for NewsCategorySelection

---
+++ NewsHW/Sezil_HW2News/Sezil_HW2News/Cells/Cells/CategorySectionCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CategorySelectionCollectionViewCell.swift
+//  Sezil_HW2News
+//
+//  Created by Sezil Akdoğan on 13.05.2023.
+//
+
+import UIKit
+
+class CategorySectionCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
